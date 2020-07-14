dab65201bcc95cb52d3b8e40f2f3dfcaa1f15411
jungdw0624 <jdw8998@daum.net>
2020-07-14 12:45:11 +0900

    Initial Commit

---
+++ MyRemiders/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRemiders
+//
+//  Created by kp_justin on 2020/07/14.
+//  Copyright © 2020 justin dongwook Jung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
